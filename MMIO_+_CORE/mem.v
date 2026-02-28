cadcc1e1ffaaa595edc04198fcffa15c06af78fe
FarmNhat <nhatpham10925@gmail.com>
2026-02-28 14:56:58 +0700

    add picorv32 core and top module for core and mem

--- MMIO_+_CORE/mem.v
+++ MMIO_+_CORE/mem.v
@@ -1,55 +1,119 @@
-module crypto_periph_top(
-    input wire clk,
-    input wire rst,
+module mem_top(
+    input  wire        clk,
+    input  wire        rst,
 
-    input wire [31:0] addr,
-    input reg [31:0] wdata_in,
-    input wire [3:0]  wstrb,
-    input wire we,
-    input wire valid,
-    output wire [31:0] rdata,
-    output wire ready
+    input  wire [31:0] addr,
+    input  wire [31:0] wdata_in,
+    input  wire [3:0]  wstrb,
+    input  wire        we,
+    input  wire        valid,
+    output reg  [31:0] rdata,
+    output reg         ready
 );
 
-    wire [31:0] r_aes, r_rsa, r_ed, r_shake, r_ram;
-    wire rd_aes, rd_rsa, rd_ed, rd_shake, rd_ram;
-
+  
     reg [31:0] wdata;
     
     always @(*) begin
+
         if (wstrb[0] == 1) wdata[7:0]   <= wdata_in[7:0];
         if (wstrb[1] == 1) wdata[15:8]  <= wdata_in[15:8];
         if (wstrb[2] == 1) wdata[23:16] <= wdata_in[23:16];
-        if (wstrb[2] == 1) wdata[31:24] <= wdata_in[31:24];
+        if (wstrb[3] == 1) wdata[31:24] <= wdata_in[31:24];
         
         //load_data_from_dmem <= mem_array[{addr_to_dmem[AddrMsb:AddrLsb]}];
     end
 
+    wire [31:0] r_aes, r_rsa, r_ed, r_shake, r_ram;
+    wire rd_aes, rd_rsa, rd_ed, rd_shake, rd_ram;
 
-    aes_gcm_mm #(.BASE_ADDR(32'h4000_0000)) 
-    u_aes (.clk(clk),.rst(rst),.addr(addr),.wdata(wdata),.we(we),.valid(valid),.rdata(r_aes),.ready(rd_aes));
+    // -------------------------
+    // Instantiate blocks
+    // -------------------------
+    aes_gcm_mm #(.BASE_ADDR(32'h4000_0000))
+    u_aes (
+        .clk(clk), .rst(rst),
+        .addr(addr),
+        .wdata(wdata),
+        .we(we),
+        .valid(valid),
+        .rdata(r_aes),
+        .ready(rd_aes)
+    );
 
-    ed25519_shake128_mm #(.BASE_ADDR(32'h4000_1000)) 
-    u_ed    (.clk(clk),.rst(rst),.addr(addr),.wdata(wdata),.we(we),.valid(valid),.rdata(r_ed),.ready(rd_ed));
-    
-    bike_mm #(.BASE_ADDR(32'h4000_2000)) 
-    u_shake (.clk(clk),.rst(rst),.addr(addr),.wdata(wdata),.we(we),.valid(valid),.rdata(r_shake),.ready(rd_shake));
-    
+    ed25519_shake128_mm #(.BASE_ADDR(32'h4000_1000))
+    u_ed (
+        .clk(clk), .rst(rst),
+        .addr(addr),
+        .wdata(wdata),
+        .we(we),
+        .valid(valid),
+        .rdata(r_ed),
+        .ready(rd_ed)
+    );
+
+    bike_mm #(.BASE_ADDR(32'h4000_2000))
+    u_bike (
+        .clk(clk), .rst(rst),
+        .addr(addr),
+        .wdata(wdata),
+        .we(we),
+        .valid(valid),
+        .rdata(r_shake),
+        .ready(rd_shake)
+    );
+
     rsa_mm #(.BASE_ADDR(32'h4000_3000))
-    u_rsa (.clk(clk),.rst(rst),.addr(addr),.wdata(wdata),.we(we),.valid(valid),.rdata(r_rsa),.ready(rd_rsa));
-    
+    u_rsa (
+        .clk(clk), .rst(rst),
+        .addr(addr),
+        .wdata(wdata),
+        .we(we),
+        .valid(valid),
+        .rdata(r_rsa),
+        .ready(rd_rsa)
+    );
+
     ram_mm #(.BASE_ADDR(32'h4000_4000))
-    u_ram (.clk(clk),.rst(rst),.addr(addr),.wdata(wdata),.we(we),.valid(valid),.rdata(r_ram),.ready(rd_ram));
+    u_ram (
+        .clk(clk), .rst(rst),
+        .addr(addr),
+        .wdata(wdata),
+        .we(we),
+        .valid(valid),
+        .rdata(r_ram),
+        .ready(rd_ram)
+    );
 
-    assign rdata =
-        rd_aes   ? r_aes   :
-        rd_rsa   ? r_rsa   :
-        rd_ed    ? r_ed    :
-        rd_shake ? r_shake :
-        rd_ram   ? r_ram   :
-        32'h0;
+    // -------------------------
+    // Safe mux
+    // -------------------------
+    always @(*) begin
+        rdata = 32'h0;
+        ready = 1'b0;
 
-    assign ready = rd_aes | rd_rsa | rd_ed | rd_shake | rd_ram;
+        if (rd_aes) begin
+            rdata = r_aes;
+            ready = 1'b1;
+        end
+        else if (rd_ed) begin
+            rdata = r_ed;
+            ready = 1'b1;
+        end
+        else if (rd_shake) begin
+            rdata = r_shake;
+            ready = 1'b1;
+        end
+        else if (rd_rsa) begin
+            rdata = r_rsa;
+            ready = 1'b1;
+        end
+        else if (rd_ram) begin
+            rdata = r_ram;
+            ready = 1'b1;
+        end
+    end
+
 
 endmodule
 
@@ -81,9 +145,9 @@ module ram_mm #(
             ready <= 1;
 
             if(we)
-                ram[addr] <= wdata;
+                ram[(addr - BASE_ADDR) >> 2] <= wdata;
             else
-                rdata <= ram[addr];
+                rdata <= ram[(addr - BASE_ADDR) >> 2];
         end
     end
 
